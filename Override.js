cd0b148c98db99f7d4b08ce265432bdedb1b8958
xlh8394 <94887643+xlh8394@users.noreply.github.com>
2024-10-05 09:28:37 +0800

    Update Override.js

--- Override.js
+++ Override.js
@@ -376,7 +376,7 @@ function main(config) {
     "DOMAIN-SUFFIX,gwdang.com,DIRECT",
     "DOMAIN-SUFFIX,iqiyi.com,DIRECT",
     "DOMAIN-KEYWORD,carryz,DIRECT",
-    "DOMAIN-KEYWORD,aliz,DIRECT",
+    "DOMAIN-KEYWORD,aliz,美国节点",
     "DOMAIN-SUFFIX,googleapis.com,AI",
     "DOMAIN-SUFFIX,gemini.google.com,AI",
     "DOMAIN-SUFFIX,sexbjcam.com,AI",
