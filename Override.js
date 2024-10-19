638d420d5df98d303603340ff0679b90fd1b8d59
xlh8394 <94887643+xlh8394@users.noreply.github.com>
2024-10-19 12:43:11 +0800

    Update Override.js

--- Override.js
+++ Override.js
@@ -198,7 +198,7 @@ function main(config) {
       "type": "url-test",
       "tolerance": 50,
       "include-all": true,
-      "interval": 600",
+      "interval": 600,
       "icon": "https://ghp.ci/https://raw.githubusercontent.com/Orz-3/mini/master/Color/Urltest.png"
     },
     {
